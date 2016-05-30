89a5bb13ebd02f308e384808e232295256679114
KarenSilva <ktrs_9303@hotmail.com>
2016-05-29 22:17:16 -0500

    se crea actividad manualidades 2

---
+++ app/src/main/java/com/example/tatisramos/ecoreciclaje/ManualidadActivity2.java
@@ -0,0 +1,13 @@
+package com.example.tatisramos.ecoreciclaje;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ManualidadActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manualidad2);
+    }
+}
